3da49b1301220b88da0d028fdf954b70a51fe365
Brevven <brevingame@gmail.com>
2022-06-04 23:27:01 -0700

    carbon added & foundry updates

--- settings-updates.lua
+++ settings-updates.lua
@@ -3,3 +3,6 @@
 -- Update various settings to give the "fullest" BZ mods experience
 data.raw["string-setting"]["bzsilicon-more-intermediates"].default_value = "more"
 data.raw["string-setting"]["bzlead-more-entities"].default_value = "yes"
+data.raw["string-setting"]["bzfoundry-plates"].default_value = "yes"
+data.raw["string-setting"]["bzcarbon-enable-carbon-black"].default_value = "yes"
+data.raw["string-setting"]["bzcarbon-reuse"].default_value = "yes"
